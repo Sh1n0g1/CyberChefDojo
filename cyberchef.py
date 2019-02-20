f8f808867aba0ce0e5619c6140536ce3cb0978fc
Sh1n0g1 <shinogi-s@macnica.net>
2019-02-20 13:39:37 +0900

    Create cyberchef.py

---
+++ cyberchef.py
@@ -0,0 +1,52 @@
+#Run CyberChef from Python using Selenium + Headless Chrome
+
+#What you need:
+# selenium (pip install selenium)
+# google-chrome (apt install google-chrome)
+# chrome web driver (http://chromedriver.chromium.org/downloads)
+#   -> wget and unzip it.
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+WEBDRIVERPATH='/usr/local/bin/chromedriver'
+CYBERCHEF_URL='https://gchq.github.io/CyberChef/'
+
+def cyberchef(input, recipe=''):
+  input=input.encode('base64').replace('=','')
+
+
+  options=Options()
+  options.add_argument('--headless')
+  options.add_argument('--disable-gpu')
+  options.add_argument('--no-sandbox')
+  service_log_path='/var/log/chromedriver.log'
+  service_args=['--verbose']
+
+  driver = webdriver.Chrome(
+    executable_path = '/usr/local/bin/chromedriver',
+    chrome_options  = options,
+    service_log_path= service_log_path,
+    service_args    = service_args
+  )
+
+  url=CYBERCHEF_URL+'#input='+input
+  if recipe!='':
+    url+='&recipe='+recipe
+  driver.get(url)
+  output=driver.find_element_by_id('output-text')
+  time.sleep(0.5)
+  result=output.get_attribute('value')
+
+  return result
+
+#sample
+
+recipe='''XOR({'option':'Hex','string':'01'},'Standard',false)'''
+input='udru032'
+success_flag=True
+
+print cyberchef(input, recipe) # 'test123'
+
+
+
